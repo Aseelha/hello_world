6bbc062de05b9aa3146bb3052b87986a69b5f3a5
aseel <aseel@172.19.136.158>
2019-02-10 07:37:34 +0200

    ygjhygj

---
+++ aseelSW/src/aseelSW/As.java
@@ -0,0 +1,17 @@
+package aseelSW;
+
+public class As {
+
+	public static void main(String[] args) {
+		swap(5,3);
+
+	}
+
+
+ public static void swap(int a, int b)
+{
+     int temp = a;
+     a = b;
+     b = temp;
+    
+}}
